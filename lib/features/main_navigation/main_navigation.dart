20fb639548db763554a1cb1c7ab5e65403b99621
Rowdife <jackpark201313@gmail.com>
2023-11-14 19:09:10 +0900

    seledtedIcon

--- lib/features/main_navigation/main_navigation.dart
+++ lib/features/main_navigation/main_navigation.dart
@@ -19,7 +19,16 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       child: Text("Home"),
     ),
     const Center(
-      child: Text("Search"),
+      child: Text("Discover"),
+    ),
+    const Center(
+      child: Text("Nothing"),
+    ),
+    const Center(
+      child: Text("Inbox"),
+    ),
+    const Center(
+      child: Text("Profile"),
     ),
   ];
 
@@ -32,6 +41,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: screens[_selectedIndex],
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
@@ -43,24 +53,28 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 text: "Home",
                 isSelected: _selectedIndex == 0,
                 icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
               ),
               NavTab(
                 text: "Discover",
                 isSelected: _selectedIndex == 1,
-                icon: FontAwesomeIcons.magnifyingGlass,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
               NavTab(
                 text: "Inbox",
                 isSelected: _selectedIndex == 3,
                 icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
               ),
               NavTab(
                 text: "Profile",
                 isSelected: _selectedIndex == 4,
                 icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
               ),
             ],
