eba8a5dc052bbcefb450ccb701d94df4da1f4cfc
Rowdife <jackpark201313@gmail.com>
2023-11-18 22:46:59 +0900

    ListView.separated

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -14,6 +15,9 @@ class _VideoCommentsState extends State<VideoComments> {
     Navigator.of(context).pop();
   }
 
+  Color bgColor = Colors.grey.shade50;
+  Color mainTextColor = Colors.grey.shade600;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,9 +25,9 @@ class _VideoCommentsState extends State<VideoComments> {
       decoration:
           BoxDecoration(borderRadius: BorderRadius.circular(Sizes.size14)),
       child: Scaffold(
-        backgroundColor: Colors.grey[50],
+        backgroundColor: bgColor,
         appBar: AppBar(
-          backgroundColor: Colors.grey[50],
+          backgroundColor: bgColor,
           automaticallyImplyLeading: false,
           title: const Text("22796 comments"),
           actions: [
@@ -33,10 +37,69 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) => Container(
-            child: const Text("I am a comment"),
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: FaIcon(FontAwesomeIcons.solidUser),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Shihyun",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: mainTextColor,
+                      ),
+                    ),
+                    Gaps.v4,
+                    const Text(
+                        "This is my golf swing. This is my golf swing. This is my golf swing. This is my golf swing. ")
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: mainTextColor,
+                  ),
+                  Gaps.v4,
+                  Text(
+                    "31.9K",
+                    style: TextStyle(
+                      color: mainTextColor,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: [
+              CircleAvatar(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.grey.shade400,
+                radius: 18,
+                child: const FaIcon(FontAwesomeIcons.solidUser),
+              ),
+            ],
           ),
         ),
       ),
