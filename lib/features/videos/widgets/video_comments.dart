59e9d029eeb9f6296144364e70e6eb5e16a88edd
Rowdife <jackpark201313@gmail.com>
2023-11-19 02:15:39 +0900

    FocusScope.of(context).unfocus();

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -18,8 +20,14 @@ class _VideoCommentsState extends State<VideoComments> {
   Color bgColor = Colors.grey.shade50;
   Color mainTextColor = Colors.grey.shade600;
 
+  void _onBodyTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       clipBehavior: Clip.hardEdge,
       decoration:
@@ -37,69 +45,122 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-            horizontal: Sizes.size16,
-          ),
-          separatorBuilder: (context, index) => Gaps.v20,
-          itemCount: 10,
-          itemBuilder: (context, index) => Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        body: GestureDetector(
+          onTap: _onBodyTap,
+          child: Stack(
             children: [
-              const CircleAvatar(
-                radius: 18,
-                child: FaIcon(FontAwesomeIcons.solidUser),
-              ),
-              Gaps.h10,
-              Expanded(
-                child: Column(
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size10,
+                  horizontal: Sizes.size16,
+                ),
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemCount: 10,
+                itemBuilder: (context, index) => Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "Shihyun",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: Sizes.size14,
-                        color: mainTextColor,
+                    const CircleAvatar(
+                      radius: 18,
+                      child: FaIcon(FontAwesomeIcons.solidUser),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Shihyun",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
+                              color: mainTextColor,
+                            ),
+                          ),
+                          Gaps.v4,
+                          const Text(
+                              "This is my golf swing. This is my golf swing. This is my golf swing. This is my golf swing. ")
+                        ],
                       ),
                     ),
-                    Gaps.v4,
-                    const Text(
-                        "This is my golf swing. This is my golf swing. This is my golf swing. This is my golf swing. ")
+                    Gaps.h10,
+                    Column(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size20,
+                          color: mainTextColor,
+                        ),
+                        Gaps.v4,
+                        Text(
+                          "31.9K",
+                          style: TextStyle(
+                            color: mainTextColor,
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
-              Gaps.h10,
-              Column(
-                children: [
-                  FaIcon(
-                    FontAwesomeIcons.heart,
-                    size: Sizes.size20,
-                    color: mainTextColor,
-                  ),
-                  Gaps.v4,
-                  Text(
-                    "31.9K",
-                    style: TextStyle(
-                      color: mainTextColor,
+              Positioned(
+                bottom: 0,
+                width: size.width,
+                child: BottomAppBar(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: Sizes.size16),
+                          child: CircleAvatar(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.grey.shade400,
+                            radius: 18,
+                            child: const FaIcon(FontAwesomeIcons.solidUser),
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            height: 40,
+                            child: TextField(
+                              expands: true,
+                              keyboardType: TextInputType
+                                  .multiline, //Doing the same thing
+                              textInputAction: TextInputAction
+                                  .newline, //Doing the same thing
+                              minLines: null,
+                              maxLines: null,
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                hintText: "Write a comment.",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size16,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Gaps.h10,
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: IconButton(
+                              onPressed: () {},
+                              icon: FaIcon(FontAwesomeIcons.paperPlane,
+                                  color: Colors.grey.shade600)),
+                        )
+                      ],
                     ),
                   ),
-                ],
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: [
-              CircleAvatar(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.grey.shade400,
-                radius: 18,
-                child: const FaIcon(FontAwesomeIcons.solidUser),
+                ),
               ),
-              const Expanded(child: TextField())
             ],
           ),
         ),
