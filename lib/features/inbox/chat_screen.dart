fe35a6696743f3d6ced6c8ac84c7097e8cc5f466
Rowdife <jackpark201313@gmail.com>
2023-11-22 01:45:14 +0900

    addItem(), removeItem()

--- lib/features/inbox/chat_screen.dart
+++ lib/features/inbox/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -12,10 +13,73 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
+  final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 300);
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(0);
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
     }
+    _items.add(_items.length);
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+          index,
+          (context, animation) => SizeTransition(
+                sizeFactor: animation,
+                child: _makeTile(index),
+              ),
+          duration: _duration);
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/76625609?v=4"),
+        child: Text("S.P"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "하나쨩($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text(
+        "다이스키~",
+      ),
+    );
   }
 
   @override
@@ -46,35 +110,7 @@ class _ChatScreenState extends State<ChatScreen> {
               scale: animation,
               child: SizeTransition(
                 sizeFactor: animation,
-                child: ListTile(
-                  leading: const CircleAvatar(
-                    radius: 30,
-                    foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/76625609?v=4"),
-                    child: Text("S.P"),
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "하나쨩($index)",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        "2:16 PM",
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontSize: Sizes.size12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: const Text(
-                    "다이스키~",
-                  ),
-                ),
+                child: _makeTile(index),
               ),
             ),
           );
