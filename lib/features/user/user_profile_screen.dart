0441269770a9b6c727a954f7f3b86c80d6f63e06
Rowdife <jackpark201313@gmail.com>
2023-11-22 18:29:36 +0900

    SliverToBoxAdapter, SliverPersistentHeader

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -32,6 +32,16 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             title: const Text("Hello!"),
           ),
         ),
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 20,
+              ),
+            ],
+          ),
+        ),
         SliverFixedExtentList(
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
@@ -42,6 +52,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               ),
             ),
             itemExtent: 100),
+        SliverPersistentHeader(
+          delegate: CustonDelegate(),
+          pinned: true,
+        ),
         SliverGrid(
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
@@ -59,5 +73,37 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ))
       ],
     );
+  }
+}
+
+class CustonDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.indigo,
+      child: const FractionallySizedBox(
+        heightFactor: 1,
+        child: Center(
+          child: Text(
+            "Title!!!",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 150;
+
+  @override
+  double get minExtent => 20;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
