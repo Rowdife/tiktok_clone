bd61c652657ec12354cec6e15c301b05d5e5c988
Rowdife <jackpark201313@gmail.com>
2023-11-24 03:46:54 +0900

    Awesome!!! ListWheelScrollView

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -6,14 +6,31 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: const Column(
-        children: [
-          CloseButton(),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Settings"),
+        ),
+        body: ListWheelScrollView(
+          useMagnifier: true,
+          magnification: 2,
+          diameterRatio: 2,
+          offAxisFraction: 2,
+          itemExtent: 200,
+          children: [
+            for (var x in [1, 2, 11, 1, 1, 1, 1, 1, 1, 23, 23, 1, 3])
+              FractionallySizedBox(
+                widthFactor: 1,
+                child: Container(
+                  color: Colors.teal,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Pick me",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+          ],
+        ));
   }
 }
