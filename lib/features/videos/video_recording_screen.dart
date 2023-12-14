d1519f20ee0b21fcd1ca6f77b9625d11fe14cd13
Rowdife <jackpark201313@gmail.com>
2023-12-15 04:19:04 +0900

    Camera package setup + initCamera

---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,112 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+  bool _deniedPermission = false;
+  bool _isSelfieMode = false;
+
+  late CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+      imageFormatGroup: ImageFormatGroup.bgra8888,
+    );
+
+    await _cameraController.initialize();
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final microphonePermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied = microphonePermission.isDenied ||
+        microphonePermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      await initCamera();
+      _hasPermission = true;
+
+      setState(() {});
+    }
+
+    if (cameraDenied || micDenied) {
+      setState(() {
+        _deniedPermission = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _deniedPermission
+          ? const Center(
+              child: Text(
+                  "You denied camera or microphone permission. If you want to use this function, you have to change permission from settings."),
+            )
+          : !_hasPermission || !_cameraController.value.isInitialized
+              ? SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Requesting permissions..."),
+                      Gaps.v20,
+                      CircularProgressIndicator.adaptive()
+                    ],
+                  ),
+                )
+              : SafeArea(
+                  child: Stack(
+                    children: [
+                      CameraPreview(_cameraController),
+                      Positioned(
+                        top: Sizes.size20,
+                        left: Sizes.size20,
+                        child: IconButton(
+                          color: Colors.white,
+                          onPressed: _toggleSelfieMode,
+                          icon: const Icon(Icons.cameraswitch),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,44 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/user/user_profile_screen.dart';
+import 'package:tiktok_clone/features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-      routes: [
-        GoRoute(
-            name: UsernameScreen.routeName,
-            path: UsernameScreen.routeURL,
-            builder: (context, state) => const UsernameScreen(),
-            routes: [
-              GoRoute(
-                name: EmailScreen.routeName,
-                path: EmailScreen.routeName,
-                builder: (context, state) {
-                  final username = state.extra as String;
-                  return EmailScreen(username: username);
-                },
-              ),
-            ]),
-      ],
-    ),
-    /*  GoRoute(
-      path: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
-    ), */
-    GoRoute(
-      path: "/users/:username",
-      builder: (context, state) {
-        final username = state.params['username'];
-        final tab = state.queryParams['show'];
-        return UserProfileScreen(username: username!, tab: tab!);
-      },
-    ),
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
+    )
   ],
 );
