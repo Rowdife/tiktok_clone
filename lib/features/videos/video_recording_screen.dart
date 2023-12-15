6a5ddd29ef2ae55dfe4880ce02d20e0120b2bc5f
Rowdife <jackpark201313@gmail.com>
2023-12-15 14:40:34 +0900

    Recording Animation

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -11,11 +11,25 @@ class VideoRecordingScreen extends StatefulWidget {
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
 
-class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin {
   bool _hasPermission = false;
   bool _deniedPermission = false;
   bool _isSelfieMode = false;
 
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 200));
+
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 0.85).animate(_buttonAnimationController);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+          vsync: this,
+          duration: const Duration(seconds: 10),
+          lowerBound: 0.0,
+          upperBound: 1.0);
   late FlashMode _flashMode;
 
   late CameraController _cameraController;
@@ -65,6 +79,14 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   void initState() {
     super.initState();
     initPermissions();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
   }
 
   Future<void> _toggleSelfieMode() async {
@@ -77,6 +99,22 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     await _cameraController.setFlashMode(newFlashMode);
     _flashMode = newFlashMode;
     setState(() {});
+  }
+
+  void _startRecording() {
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  void _stopRecording() {
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _progressAnimationController.dispose();
   }
 
   @override
@@ -103,6 +141,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                 )
               : SafeArea(
                   child: Stack(
+                    alignment: Alignment.center,
                     children: [
                       CameraPreview(_cameraController),
                       Positioned(
@@ -144,10 +183,48 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                               color: _flashMode == FlashMode.torch
                                   ? Colors.yellow
                                   : Colors.white,
-                              onPressed: () => _setFlashMode(FlashMode.torch),
+                              onPressed: () async {
+                                if (_isSelfieMode) return;
+                                _setFlashMode(FlashMode.torch);
+                              },
                               icon: const Icon(Icons.flashlight_on_rounded),
                             ),
                           ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: Sizes.size40,
+                        child: GestureDetector(
+                          onTapDown: (details) => _startRecording(),
+                          onTapUp: (details) => _stopRecording(),
+                          child: ScaleTransition(
+                            scale: _buttonAnimation,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                SizedBox(
+                                  width: Sizes.size80 + Sizes.size4,
+                                  height: Sizes.size80 + Sizes.size4,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.red.shade400,
+                                    value: _progressAnimationController.value,
+                                  ),
+                                ),
+                                Container(
+                                  width: Sizes.size80,
+                                  height: Sizes.size80,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.red.shade400,
+                                    border: Border.all(
+                                      color: Colors.white,
+                                      width: 5,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ],
