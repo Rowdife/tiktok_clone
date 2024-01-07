c032bbb4f7f7c73f43470e48c3c43d5dd00bd1a6
Rowdife <jackpark201313@gmail.com>
2024-01-07 09:36:56 +0900

    signin with github

--- lib/features/authentication/repos/authentication_repo.dart
+++ lib/features/authentication/repos/authentication_repo.dart
@@ -24,6 +24,10 @@ class AuthenticationRepository {
       password: password,
     );
   }
+
+  Future<void> githubSignIn() async {
+    await _firebaseAuth.signInWithProvider(GithubAuthProvider());
+  }
 }
 
 final authRepo = Provider((ref) => AuthenticationRepository());
