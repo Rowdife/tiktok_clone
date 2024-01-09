bb14bc89fb7fce89759f862d1ab8db142c346528
Rowdife <jackpark201313@gmail.com>
2024-01-09 19:16:10 +0900

    createProfile();

--- lib/features/user/view_models/users_view_model.dart
+++ lib/features/user/view_models/users_view_model.dart
@@ -3,29 +3,36 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/user/models/user_profile_model.dart';
+import 'package:tiktok_clone/features/user/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
+  late final UserRepository _repository;
+
   @override
   FutureOr<UserProfileModel> build() {
+    _repository = ref.read(userRepo);
     return UserProfileModel.empty();
+    // This return is to initialize the UserProfileModel as an empty model so that we can reuse this vm next time.
+    // If we do not initialize it as empty, you imagine, there will be some previous information in there right?
   }
 
   Future<void> createProfile(UserCredential credential) async {
     if (credential.user == null) {
       throw Exception("Account not created");
     }
-    state = AsyncValue.data(
-      UserProfileModel(
-        bio: "undefined",
-        link: "undefined",
-        email: credential.user!.email ?? "anon@anon.com",
-        uid: credential.user!.uid,
-        name: credential.user!.displayName ?? "Anonymous",
-      ),
+    state = const AsyncValue.loading();
+    final profile = UserProfileModel(
+      bio: "undefined",
+      link: "undefined",
+      email: credential.user!.email ?? "anon@anon.com",
+      uid: credential.user!.uid,
+      name: credential.user!.displayName ?? "Anonymous",
     );
+    await _repository.createProfile(profile);
+    state = AsyncValue.data(profile);
   }
 }
 
-final usersProvider = AsyncNotifierProvider(
+final usersProvider = AsyncNotifierProvider<UsersViewModel, UserProfileModel>(
   () => UsersViewModel(),
 );
